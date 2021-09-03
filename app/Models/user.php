2f327a9d0866967ca85c87029c3392d028f459e7
Kanu Peace <kanupeace85@gmail.com>
2021-09-03 08:54:26 +0100

    verification

--- app/Models/user.php
+++ app/Models/user.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model
 {
     
     use HasFactory;
+    use Notifiable;
     protected $fillable = [
         'name',
         'email',
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -21,7 +21,35 @@
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
-            </div>
+            </div>@extends("auth.layouts.app" , ["meta_title" => "Verify Email"])
+            @section('content')
+
+
+                <h1 class="title">Verify Email</h1>
+
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
+                @endif
+
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }},
+                <form action="{{ route('verification.resend') }}" method="POST" class="subscription-form form-row">
+                    @csrf
+
+
+                    <div class="col-12 row mb-4 mt-2">
+                        <div class="col-md-6">
+                            <button type="submit" class="axil-button button-rounded"><span>Click to resend</span></button>
+                        </div>
+                    </div>
+
+                </form>
+
+
+            @endsection
+
         </div>
     </div>
 </div>
